3ada87c09c133d56adfaed1da9d8b9f56f5e893b
Ahmad Ullah <114727856+Ahmad264@users.noreply.github.com>
2025-06-28 10:21:55 +0530

    Update app.js

--- BuildPage/app.js
+++ BuildPage/app.js
@@ -5,35 +5,38 @@ const connectDB = require('./config/db');
 // Routes
 const miniRouter = require('./routes/mini');
 const projectRoute = require('./routes/Project');
-const majorProjectRoutes = require("./routes/major");
+const majorProjectRoutes = require('./routes/major');
 const bookingRouter = require('./routes/Booking');
 const midProjectRouter = require('./routes/midProjectRoutes');
+const transactionRoutes = require('./routes/transactionRoutes');
 
 const app = express();
 
-app.use(cors()); 
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Connect MongoDB
 connectDB();
 
-// Routes
+// Use Routes
 app.use('/api', miniRouter);
 app.use('/api', midProjectRouter);
-app.use("/api", majorProjectRoutes);
+app.use('/api', majorProjectRoutes);
 app.use('/api', bookingRouter);
 app.use('/api', projectRoute);
+app.use('/api', transactionRoutes);
 
 // 404 Handler
 app.use((req, res) => {
-    res.status(404).json({ message: 'Not found' });
+  res.status(404).json({ message: 'Not found' });
 });
 
 // Error Handler
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ message: 'Server Error' });
+  console.error(err.stack);
+  res.status(500).json({ message: 'Server Error' });
 });
 
-// Start server
+// Start Server
 app.listen(5000, () => console.log('✅ Server running on port 5000.'));
